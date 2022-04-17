09f2295182c1cfecd29facb4339fbf9637061230
Ridvan <103427078+ridvanms@users.noreply.github.com>
2022-04-17 18:54:25 +0300

    Create AnimalType.js

---
+++ Conditional Statements Advanced - Lab/AnimalType.js
@@ -0,0 +1,12 @@
+function animalType(input) {
+  let type = input[0];
+
+  if (type === "dog") {
+    console.log("mammal");
+  } else if (type === "crocodile" || type === "tortoise" || type === "snake") {
+    console.log("reptile");
+  } else {
+    console.log("unknown");
+  }
+}
+animalType(["dog"]);
