4f6562f99fe4190be923c00a141b3dd36ea5336f
Ridvan <103427078+ridvanms@users.noreply.github.com>
2022-04-17 17:44:13 +0300

    Create Number 100...200.js

---
+++ Conditional Statements - Lab/Number 100...200.js
@@ -0,0 +1,12 @@
+function lessBetweenBigger(input) {
+  let number = Number(input[0]);
+
+  if (number > 200) {
+    console.log("Greater than 200");
+  } else if (number >= 100 && number <= 200) {
+    console.log("Between 100 and 200");
+  } else if (number < 100) {
+    console.log("Less than 100");
+  }
+}
+lessBetweenBigger(["210"]);
