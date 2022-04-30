35fc94de76438fc4633d95f910ed344b5cf49a34
Ridvan <ridvans@abv.bg>
2022-04-30 19:09:12 +0300

    Update fishingBoat.js
    changing to new way

--- Conditional Statements Advanced - Exercise/fishingBoat.js
+++ Conditional Statements Advanced - Exercise/fishingBoat.js
@@ -1,46 +1,39 @@
 function fishingBoat(input) {
-  let budget = +input[0];
+  let budget = Number(input[0]);
   let season = input[1];
-  let fishermenCount = +input[2];
+  let fishermenCounts = Number(input[2]);
 
-  // rent prices
-  let rentPrice;
+  // rentPrices
+  let rent = {
+    Spring: 3000,
+    Summer: 4200,
+    Autumn: 4200,
+    Winter: 2600,
+  };
+  // calculating the price with discounts if there have some
+  let priceWithDiscount;
 
-  // looking for what is the season
-  if (season === "Spring") {
-    rentPrice = 3000;
-  } else if (season === "Summer" || season === "Autumn") {
-    rentPrice = 4200;
-  } else if (season === "Winter") {
-    rentPrice = 2600;
+  if (fishermenCounts <= 6) {
+    priceWithDiscount = rent[`${season}`] * 0.1;
+  } else if (fishermenCounts <= 11) {
+    priceWithDiscount = rent[`${season}`] * 0.15;
+  } else {
+    priceWithDiscount = rent[`${season}`] * 0.25;
   }
+  // bonuse discount if there has some
+  priceWithDiscount +=
+    fishermenCounts % 2 === 0 && season !== "Autumn"
+      ? (rent[`${season}`] - priceWithDiscount) * 0.05
+      : 0;
 
-  // discounts
-  let discountPrice = 0;
+  // total price
+  let totalPrice = rent[`${season}`] - priceWithDiscount;
 
-  if (fishermenCount <= 6) {
-    discountPrice = 0.1 * rentPrice;
-  } else if (fishermenCount > 6 && fishermenCount <= 11) {
-    discountPrice = 0.15 * rentPrice;
-  } else if (fishermenCount > 11) {
-    discountPrice = 0.25 * rentPrice;
-  }
-
-  // bonus discount
-  if (fishermenCount % 2 === 0 && season !== "Autumn") {
-    discountPrice += (rentPrice - discountPrice) * 0.05;
-  }
-
-  //calculating the total price
-  let totalPrice = rentPrice - discountPrice;
-
-  // watching for enough budget
-  if (budget >= totalPrice) {
-    console.log(`Yes! You have ${(budget - totalPrice).toFixed(2)} leva left.`);
-  } else if (budget < totalPrice) {
-    console.log(
-      `Not enough money! You need ${(totalPrice - budget).toFixed(2)} leva.`
-    );
-  }
+  // if there have enough budget
+  let result =
+    budget >= totalPrice
+      ? `Yes! You have ${(budget - totalPrice).toFixed(2)} leva left.`
+      : `Not enough money! You need ${(totalPrice - budget).toFixed(2)} leva.`;
+  console.log(result);
 }
 fishingBoat(["3000", "Summer", "12"]);
