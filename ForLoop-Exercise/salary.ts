6f0dd33ef05188e47aed818ab7a724307f142113
Ridvan <ridvans@abv.bg>
2022-05-09 16:44:59 +0300

    Create salary.ts
    needs for fixing

---
+++ ForLoop-Exercise/salary.ts
@@ -0,0 +1,16 @@
+function solving(input){
+
+}
+solving(['10','750','Facebook','dev.bg','Instagram','Facebook'])
+
+function testing(){
+    let num = 10;
+    for(num;num !== 0;num--){
+        console.log(num)
+        if(num === 5){
+            console.log(`last num`)
+            return;
+        }
+    }
+}
+testing();
