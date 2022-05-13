87e18a62c23ddf9b418f559363003b8adbd7671c
Ridvan <ridvans@abv.bg>
2022-05-13 21:52:11 +0300

    Update salary.ts
    Refactoring!

--- ForLoop-Exercise/salary.ts
+++ ForLoop-Exercise/salary.ts
@@ -1,16 +1,24 @@
-function solving(input){
-
-}
-solving(['10','750','Facebook','dev.bg','Instagram','Facebook'])
-
-function testing(){
-    let num = 10;
-    for(num;num !== 0;num--){
-        console.log(num)
-        if(num === 5){
-            console.log(`last num`)
-            return;
-        }
+function salary(input){
+    let openTabs = Number(input[0]);
+    let salary = Number(input[1]);
+    
+    // calculating the penalties
+    for (let i = 2;i <= input.length;i++){
+       switch(input[i]){
+           case "Facebook":
+               salary -= 150;
+               break;
+            case "Instagram":
+                salary -= 100;
+                break;
+            case "Reddit":
+                salary -= 50;
+                break;
+       }
+       if(salary <= 0 ){
+           console.log(`You have lost your salary.`);
+           return;
+       }
     }
+    console.log(salary); 
 }
-testing();
