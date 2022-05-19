453f6eb71fa50ab97f54718b5d5cfc8b06c2adee
Ridvan <ridvans@abv.bg>
2022-05-19 22:00:53 +0300

    Update PasswordGuess.js

--- Conditional Statements - Lab/PasswordGuess.js
+++ Conditional Statements - Lab/PasswordGuess.js
@@ -1,8 +1,8 @@
 function guessing(input) {
-  let unknown = input[0];
-  const known = "s3cr3t!P@ssw0rd";
+  let input = input[0];
+  const password = "s3cr3t!P@ssw0rd";
 
-  let result = unknown == known ? "Welcome" : "Wrong password!";
+  let result = input === password ? "Welcome" : "Wrong password!";
   console.log(result);
 }
 guessing(["s3cr3t!P@ssw0rd"]);
