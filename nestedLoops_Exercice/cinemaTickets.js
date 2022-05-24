5ed1e86cadb276b77a405f893dc2dc92d181c99d
Ridvan <ridvans@abv.bg>
2022-05-24 11:27:49 +0300

    updating the cinema tickects with nestedLoops

--- nestedLoops_Exercice/cinemaTickets.js
+++ nestedLoops_Exercice/cinemaTickets.js
@@ -1,56 +1,41 @@
 function tickets(input) {
-    let totalTickets = 0;
+    let index = 0
+    let command = input[index];
 
-    let studentTickets = 0;
-    let standardTickets = 0;
-    let kidsTickets = 0;
-
-    let index = 0;
-    while (input[index] !== 'Finish') {
-        input[index] === 'End' ? index++ : '';
-        let moveName = input[index];
+    let totalTicketsCount = 0;
+    let studentTicketsCount = 0;
+    let standardTicketsCount = 0;
+    let kidTicketsCount = 0;
+    while (command !== 'Finish') {
+        let moveName = command;
         index++;
         let places = +input[index];
         index++;
-        let tookedPlaces = 0;
+        let soldTickets = 0;
         while (input[index] !== 'End') {
-            let temp = input[index]
-            totalTickets++;
-            tookedPlaces++;
-            if (temp === 'student') {
-                studentTickets++;
-            } else if (temp === 'standard') {
-                standardTickets++;
-            } else if (temp === 'kid') {
-                kidsTickets++;
+            let tempCommand = input[index];
+            totalTicketsCount++;
+            soldTickets++;
+            switch (tempCommand) {
+                case 'student': studentTicketsCount++; break;
+                case 'standard': standardTicketsCount++; break;
+                case 'kid': kidTicketsCount++; break;
             }
-            if (places <= tookedPlaces) {
+            if (places <= soldTickets) {
                 break;
             }
             index++;
         }
-        console.log(`${moveName} - ${(tookedPlaces / places * 100).toFixed(2)}% full.`)
+        index++;
+        command = input[index];
+        console.log(`${moveName} - ${(soldTickets / places * 100).toFixed(2)}% full.`)
     }
-    console.log(`Total tickets: ${totalTickets}`)
-    console.log(`${(studentTickets / totalTickets * 100).toFixed(2)}% student tickets.`)
-    console.log(`${(standardTickets / totalTickets * 100).toFixed(2)}% standard tickets.`)
-    console.log(`${(kidsTickets / totalTickets * 100).toFixed(2)}% kids tickets.`)
+    console.log(`Total tickets: ${totalTicketsCount}`)
+
+    console.log(`${(studentTicketsCount / totalTicketsCount * 100).toFixed(2)}% student tickets.`)
+    console.log(`${(standardTicketsCount / totalTicketsCount * 100).toFixed(2)}% standard tickets.`)
+    console.log(`${(kidTicketsCount / totalTicketsCount * 100).toFixed(2)}% kids tickets.`)
 }
-tickets(["Taxi",
-    "10",
-    "standard",
-    "kid",
-    "student",
-    "student",
-    "standard",
-    "standard",
-    "End",
-    "Scary Movie",
-    "6",
-    "student",
-    "student",
-    "student",
-    "student",
-    "student",
-    "student",
-    "Finish"])
+tickets(['Taxi', '10', 'standard', 'kid', 'student',
+    'student', 'standard', 'standard', 'End', 'Scary Movie',
+    '6', 'student', 'student', 'student', 'student', 'student', 'student', 'Finish']);
